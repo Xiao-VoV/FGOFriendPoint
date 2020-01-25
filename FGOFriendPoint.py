07c175a66b41cb3d7542a266e9dda440704c9b3e
windowsxp <1679190840@qq.com>
2020-01-25 17:23:28 +0800

    新增adb文件

--- FGOFriendPoint.py
+++ FGOFriendPoint.py
@@ -1,19 +1,24 @@
 import os
 import  time
-# adb connect 127.0.0.1:7555
+# adb connect 127.0.0.1:7555 
 
 # 931.4 622.2
 # 861.4 753
 
 # adb shell input tap 924.4 629.2
 
-os.system('adb connect 127.0.0.1:7555')
-time.sleep(3)
-i = 0
-for i in range(101):
-    for n in range(8):
-        os.system('adb shell input tap 930.4 622.2')
-        time.sleep(0.2)
-    os.system('adb shell input tap 861.4 753')
-    time.sleep(0.3)
-    print(f"第 {i+1} 次抽卡！")
+if os.system('adb connect 127.0.0.1:7555') != 0:
+    print("连接错误！")
+else:
+    time.sleep(3)
+    print("开始抽卡--->")
+    i = 0
+    for i in range(50):
+        for n in range(8):
+            os.system('adb shell input tap 930.4 622.2')
+            time.sleep(0.2)
+        os.system('adb shell input tap 861.4 753')
+        time.sleep(0.3)
+        print(f"第 {i+1} 次抽卡！")
+
+os.system('adb kill-server')
