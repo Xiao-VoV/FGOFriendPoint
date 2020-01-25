7740e257a075f433568eba2daf56114c30d6a08a
windowsxp <1679190840@qq.com>
2020-01-25 13:57:21 +0800

    添加md

--- FGOFriendPoint.py
+++ FGOFriendPoint.py
@@ -7,13 +7,13 @@ import  time
 
 # adb shell input tap 924.4 629.2
 
-#os.system('adb connect 127.0.0.1:7555')
-#time.sleep(6)
+os.system('adb connect 127.0.0.1:7555')
+time.sleep(3)
 i = 0
 for i in range(101):
     for n in range(8):
         os.system('adb shell input tap 930.4 622.2')
         time.sleep(0.2)
     os.system('adb shell input tap 861.4 753')
-    time.sleep(0.5)
+    time.sleep(0.3)
     print(f"第 {i+1} 次抽卡！")
