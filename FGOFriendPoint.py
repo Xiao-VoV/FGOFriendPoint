7a67402542f35d17a729262c36ef47ee531b6b79
windowsxp <1679190840@qq.com>
2020-01-29 11:50:08 +0800

    Update

--- FGOFriendPoint.py
+++ FGOFriendPoint.py
@@ -10,15 +10,16 @@ import  time
 if os.system('adb connect 127.0.0.1:7555') != 0:
     print("连接错误！")
 else:
-    time.sleep(3)
+    time.sleep(1)
     print("开始抽卡--->")
     i = 0
-    for i in range(50):
-        for n in range(7):
+    for i in range(100):
+        for n in range(8):
             os.system('adb shell input tap 930.4 622.2')
-            time.sleep(0.2)
+            time.sleep(0.18)
+        time.sleep(0.1)
         os.system('adb shell input tap 861.4 753')
-        time.sleep(0.3)
+        time.sleep(0.1)
         print(f"第 {i+1} 次抽卡！")
 print("抽卡结束！")
 os.system('adb kill-server')
