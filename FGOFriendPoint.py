1fb184db2792a118b40b6d22c6efe9a1a563d273
XP <1679190840@qq.com>
2020-04-15 08:57:48 +0800

    少量优化，支持1.66

--- FGOFriendPoint.py
+++ FGOFriendPoint.py
@@ -20,24 +20,25 @@ import  time
 #        print(f"第 {i+1} 次抽卡！")
 
 
-
-print("在<继续进行10次召唤>界面处运行程序!\n")
-print("----------------------------\n")
+print("----------------------------")
+print("在<继续进行10次召唤>界面处运行程序!")
+print("----------------------------")
 if os.system('adb connect 127.0.0.1:7555') != 0:
     print("连接错误！")
 else:
-    print("*******开始抽卡*******\n")
+    print("*******开始抽卡*******")
     i = 0
-    for i in range(100):
+    count  = int(input("输入要抽卡的次数:>"))
+    for i in range(count):
         os.system('adb shell input tap 852.4 761.0')
-        time.sleep(0.1)
+        #time.sleep(0.05)
         os.system('adb shell input tap 937.2 637.2')
         j = 0
         time.sleep(2)
-        for j in range(4):
+        for j in range(3):
             os.system('adb shell input tap 1288.0 599.2')
             time.sleep(0.13)
-        time.sleep(0.25)
+        time.sleep(0.3)
         print(f"第 {i+1} 次抽卡！")
 
 
